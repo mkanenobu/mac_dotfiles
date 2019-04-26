1b0772ce431a8887cacb82e3252a851f6b34fd0b
Mitsuru Kanenobu <kaneda.1018@gmail.com>
2019-04-26 16:13:24 +0900

    Fri Apr 26 16:13:24 JST 2019

---
+++ language_update.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+cols="$(tput cols)"
+separater="$(echo "===================================================================================================================================" | cut -c "1-${cols}")"
+
+echo "${separater}"
+echo "Nimble"
+echo "${separater}"
+nimble update
+
+echo 
+echo "${separater}"
+echo "Rustup"
+echo "${separater}"
+rustup update
+
+echo 
+echo "${separater}"
+echo "Yarn"
+echo "${separater}"
+yarn global upgrade
+
+echo 
+echo "${separater}"
+echo "Opam"
+echo "${separater}"
+opam update && opam upgrade && eval $(opam env)
