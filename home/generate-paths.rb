fc894e4f75bf2f2152b8aa75c0431c1f27a0626d
mkanenobu <mitsuru.kanenobu@gmail.com>
2025-04-29 11:02:44 +0900

    Tue Apr 29 11:02:44 JST 2025

--- home/generate-paths.rb
+++ home/generate-paths.rb
@@ -8,25 +8,16 @@ paths = [
   "/opt/homebrew/bin",
   "$HOME/.nimble/bin",
   "$HOME/.cargo/bin",
-  "$HOME/.nodenv/bin",
   "$HOME/go/bin",
   "$HOME/.local/toolbox",
   "$HOME/.local/bin",
 
-  # Rancher Desktop
-  "$HOME/.rd/bin",
   # Android
   "$HOME/Library/Android/sdk/platform-tools",
   # Google Cloud SDK
   "$HOME/google-cloud-sdk/bin",
-  # rebar3 (for erlang)
-  "$HOME/.cache/rebar3/bin",
-  # coursier (for scala)
-  "$HOME/Library/Application Support/Coursier/bin",
-  # moonbit
-  "$HOME/.moon/bin",
-  # elan, lean 4 version manager
-  "$HOME/.elan/bin",
+  # Flutter
+  "$HOME/.flutter/bin",
 ]
 
 # Replace "$HOME" with the actual home directory path
